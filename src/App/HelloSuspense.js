95298861b3c939ffd73b716eec44bc03d2aeb659
Jikku Jose <jikkujose@gmail.com>
2018-11-17 22:56:57 +0530

    Setup base UI

---
+++ src/App/HelloSuspense.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export function HelloSuspense() {
+  return (
+    <>
+      <div className="sans-serif white pa3 bg-black-80 w5 br2">
+        SVG Element as React Component!
+      </div>
+    </>
+  )
+}
--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react"
+import { HelloSuspense } from "./HelloSuspense"
 
 export class App extends Component {
   render() {
     return (
-      <>
-        <div>hello</div>
-      </>
+      <div className="f ch bg-yellow">
+        <div className="" style={{ width: 800 }}>
+          <div className="f2 sans-serif fw2 pa3 mb2">
+            Hello Hooks, SVG React Component & Suspsene
+          </div>
+          <div className="pa3">
+            <HelloSuspense />
+          </div>
+        </div>
+      </div>
     )
   }
 }
